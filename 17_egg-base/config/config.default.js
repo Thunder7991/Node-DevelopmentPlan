9e00be863581fdd25a9067090c8b0b452aeb33b3
Coxzero <853524319@qq.com>
2023-01-23 21:20:12 +0800

    feat(egg-base): use of the egg-mongoose plug-in in the egg project

---
+++ 17_egg-base/config/config.default.js
@@ -0,0 +1,40 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1674478514793_3560';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  config.mongoose = {
+    client: {
+      url: 'mongodb://192.168.148.128:27017/express-video',
+      options: {},
+      // mongoose global plugins, expected a function or an array of function and options
+      plugins: [],
+    },
+  };
+
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
---
+++ 17_egg-base/config/plugin.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+
+
+module.exports.mongoose = {
+  enable: true,
+  package: 'egg-mongoose',
+};

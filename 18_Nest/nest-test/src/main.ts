84eb7df72dcdad64563efa1b2f21ca34917f8669
Coxzero <853524319@qq.com>
2023-01-26 22:27:07 +0800

    feat(nest): nest project initialization (incomplete)

---
+++ 18_Nest/nest-test/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000, () => {
+    console.log('http://127.0.0.1:3000');
+  });
+}
+bootstrap();

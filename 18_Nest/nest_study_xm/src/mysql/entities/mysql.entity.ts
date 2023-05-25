e5a6c14b1618504a1e2e4160da9d7240ea2283e3
Coxzero <853524319@qq.com>
2023-05-25 23:44:52 +0800

    feat(entity): add Entity

--- 18_Nest/nest_study_xm/src/mysql/entities/mysql.entity.ts
+++ 18_Nest/nest_study_xm/src/mysql/entities/mysql.entity.ts
@@ -1,11 +1,21 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Generated,
+} from 'typeorm';
 
 @Entity()
 export class Mysql {
-  @PrimaryGeneratedColumn()
+  //获取uuid ,只需传入 uuid 即可
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
   name: string;
 
   @Column()
@@ -13,4 +23,24 @@ export class Mysql {
 
   @Column()
   age: number;
+  @CreateDateColumn({
+    type: 'timestamp',
+  })
+  createTime: Date;
+
+  @Generated('uuid')
+  uuid: string;
+  @Column({
+    type: 'enum',
+    enum: [1, 2, 3],
+    default: 1,
+    select: true, //不会返回给用户
+    comment: '注释', //
+  })
+  thunder: number;
+
+  @Column('simple-array')
+  names: string[];
+  @Column('simple-json')
+  json: { name: string; age: number };
 }

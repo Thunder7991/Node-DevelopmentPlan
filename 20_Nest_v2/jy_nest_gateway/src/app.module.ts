965ccae6af0d558daeff9059f2ce75addfa88e99
Coxzero <853524319@qq.com>
2023-05-30 00:06:09 +0800

    fix(cachemodule): :ambulance: Modify the type verification

--- 20_Nest_v2/jy_nest_gateway/src/app.module.ts
+++ 20_Nest_v2/jy_nest_gateway/src/app.module.ts
@@ -5,14 +5,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { getConfig } from '@/utils';
 import { UserModule } from './user/user.module';
-import { CacheModule } from '@nestjs/cache-manager';
-const redisStore = require('cache-manager-redis-store');
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     CacheModule.register({
       isGlobal: true,
-      store: redisStore,
+      store: redisStore as unknown as CacheStore,
       host: getConfig('REDIS_CONFIG').host,
       port: getConfig('REDIS_CONFIG').port,
       auth_pass: getConfig('REDIS_CONFIG').auth,

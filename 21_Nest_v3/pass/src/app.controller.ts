37a3182e30942f5e39bba242d5a23901bc40e524
thunder <853524319@qq.com>
2023-06-29 23:50:35 +0800

    feat(provider): :ambulance: 添加多种provider

--- 21_Nest_v3/pass/src/app.controller.ts
+++ 21_Nest_v3/pass/src/app.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject('app_service') private readonly appService: AppService,
+    @Inject('pserson') private readonly person: { name: string; age: number },
+    @Inject('pserson2')
+    private readonly person2: { name: string; desc: string },
+    @Inject('pserson5')
+    private readonly persion5: { name: string; desc: string },
+  ) {}
 
   @Get()
   getHello(): string {
-    debugger;
+    // debugger;
+    console.log(this.person);
+    console.log(this.person2);
+    console.log(this.persion5);
 
     return this.appService.getHello();
   }
--- 21_Nest_v3/pass/src/app.module.ts
+++ 21_Nest_v3/pass/src/app.module.ts
@@ -6,6 +6,69 @@ import { PersonModule } from './person/person.module';
 @Module({
   imports: [PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  // providers: [AppService],
+  providers: [
+    {
+      provide: 'app_service',
+      useClass: AppService,
+    },
+    {
+      provide: 'person',
+      useValue: {
+        name: 'aaa',
+        age: 20,
+      },
+    },
+    //工厂
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: 'bbb',
+          desc: 'ccc',
+        };
+      },
+    },
+    {
+      //等于注入这两个 provider
+      provide: 'person3',
+      useFactory(person: { name: string }, appService: AppService) {
+        return {
+          name: person.name,
+          desc: appService.getHello(),
+        };
+      },
+      inject: ['person', AppService],
+    },
+    //上面等同于
+    // [
+    //   AppService,
+    //   {
+    //     provide: 'pserson',
+    //     useValue: {
+    //       name: 'aaa',
+    //       age: 20,
+    //     },
+    //   },
+    // ],
+
+    // useFactory 支持异步：
+
+    {
+      provide: 'person5',
+      // 通过useExisting指定别名
+      useExisting: 'person6',
+      async useFactory() {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 3000);
+        });
+
+        return {
+          name: 'bbb',
+          desc: 'ccc',
+        };
+      },
+    },
+  ],
 })
 export class AppModule {}

ab355b75eaf8af7daaaca88acb8dacf1194b420c
thunder <853524319@qq.com>
2023-08-01 00:19:52 +0800

    perf(alc adding redis): :ambulance: 添加redis缓存

--- 21_Nest_v3/acl-test/src/user/permission.guard.ts
+++ 21_Nest_v3/acl-test/src/user/permission.guard.ts
@@ -10,6 +10,7 @@ import { Request } from 'express';
 import { UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Reflector } from '@nestjs/core';
+import { RedisService } from 'src/redis/redis.service';
 
 @Injectable()
 export class PermissionGuard implements CanActivate {
@@ -19,6 +20,9 @@ export class PermissionGuard implements CanActivate {
   private jwtService: JwtService;
   @Inject(Reflector)
   private reflector: Reflector;
+
+  @Inject(RedisService)
+  private redisService: RedisService;
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request: Request = context.switchToHttp().getRequest();
     const authorization = request.headers.authorization || '';
@@ -26,14 +30,27 @@ export class PermissionGuard implements CanActivate {
     if (!bearer || bearer.length < 2) {
       throw new UnauthorizedException('用户未登录');
     }
+
     const token = bearer[1];
 
     const info = this.jwtService.verify(token);
+    let permissions = await this.redisService.listGet(
+      `user_${info.user.username}_permissions`,
+    );
 
-    const foundUser = await this.userService.findByUsername(info.user.username);
-    console.log(37, context.getHandler());
+    if (!permissions) {
+      const foundUser = await this.userService.findByUsername(
+        info.user.username,
+      );
+      permissions = foundUser.permissions.map((item) => item.name);
+      this.redisService.listSet(
+        `user_${info.user.username}_permissions`,
+        permissions,
+        60 * 30,
+      );
+    }
     const permission = this.reflector.get('permission', context.getHandler());
-    if (foundUser.permissions.some((item) => item.name === permission)) {
+    if (permissions.some((item) => item === permission)) {
       return true;
     } else {
       console.log(42);

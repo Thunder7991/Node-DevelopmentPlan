d06b432602dfb040f062ca78d6938c4696e288d6
thunder <853524319@qq.com>
2023-08-05 10:18:25 +0800

    docs(load balance): :memo: nginx add

--- 21_Nest_v3/nginx-nest-app/src/main.ts
+++ 21_Nest_v3/nginx-nest-app/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //添加全局API
+  app.setGlobalPrefix('/api');
+
   await app.listen(3000);
 }
 bootstrap();

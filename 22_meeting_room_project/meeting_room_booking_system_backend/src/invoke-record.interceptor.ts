67d125d777c554a98ac51236a68e44c21cac6c3a
thunderchen <853524319@qq.com>
2023-12-09 14:07:07 +0800

    feat(logger): add global logger

---
+++ 22_meeting_room_project/meeting_room_booking_system_backend/src/invoke-record.interceptor.ts
@@ -0,0 +1,55 @@
+/*
+ * @Author: thunderchen
+ * @Date: 2023-12-09 13:41:43
+ * @LastEditTime: 2023-12-09 13:53:38
+ * @email: 853524319@qq.com
+ * @Description:接口访问记录：记录下访问的 ip、user agent、请求的 controller、method，接口耗时、响应内容，当前登录用户等信息。
+ */
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { Observable, tap } from 'rxjs';
+
+@Injectable()
+export class InvokeRecordInterceptor implements NestInterceptor {
+  private readonly logger = new Logger(InvokeRecordInterceptor.name);
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse<Response>();
+
+    const userAgent = request.headers['user-agent'];
+    const { ip, method, path } = request;
+
+    this.logger.debug(
+      `${method} ${path} ${ip} ${userAgent}: ${context.getClass().name} ${
+        context.getHandler().name
+      } invoked...`,
+    );
+
+    this.logger.debug(
+      `user: ${request.user?.userId}, ${request.user?.username}`,
+    );
+
+    const now = Date.now();
+
+    return next.handle().pipe(
+      tap((res) => {
+        this.logger.debug(
+          `${method} ${path} ${ip} ${userAgent}: ${response.statusCode}: ${
+            Date.now() - now
+          }ms`,
+        );
+        this.logger.debug(`Response: ${JSON.stringify(res)}`);
+      }),
+    );
+  }
+}
--- 22_meeting_room_project/meeting_room_booking_system_backend/src/main.ts
+++ 22_meeting_room_project/meeting_room_booking_system_backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { FormatResponseInterceptor } from './format-response.interceptor';
+import { InvokeRecordInterceptor } from './invoke-record.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   //全局启用响应拦截器
   app.useGlobalInterceptors(new FormatResponseInterceptor());
+  //全局启用logger
+  app.useGlobalInterceptors(new InvokeRecordInterceptor())
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('nest_server_port'));

9d53540deff2a5e95ca18d8897690ae3793c0a30
thunderchen <853524319@qq.com>
2023-12-09 13:41:19 +0800

    feat(response interceptor): add Response interceptor

---
+++ 22_meeting_room_project/meeting_room_booking_system_backend/src/format-response.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { map, Observable } from 'rxjs';
+
+// 修改响应内容的拦截器
+@Injectable()
+export class FormatResponseInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response = context.switchToHttp().getResponse<Response>();
+
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          code: response.statusCode,
+          message: 'success',
+          data,
+        };
+      }),
+    );
+  }
+}
--- 22_meeting_room_project/meeting_room_booking_system_backend/src/main.ts
+++ 22_meeting_room_project/meeting_room_booking_system_backend/src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { FormatResponseInterceptor } from './format-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //对请求体做校验 全局启用
   app.useGlobalPipes(new ValidationPipe());
+  //全局启用响应拦截器
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('nest_server_port'));
